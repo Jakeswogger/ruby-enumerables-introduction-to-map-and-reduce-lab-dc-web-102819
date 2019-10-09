2b20cd670fbab9ade0461cd62673e93cd7680f6f
Jake Swogger <jakeswogger1@gmail.com>
2019-10-09 20:37:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,13 +39,6 @@ end
 new_array
 end
 
-def reduce_to_total(source_array, starting_point=0)
-  new_array = []
-  new_array = source_array.reduce(starting_point) do |a, add| a+add
-end
-new_array
-end
-
 def reduce_to_all_true(source_array)
   new_array 
   new_array = source_array.reduce(0) do |a| 
